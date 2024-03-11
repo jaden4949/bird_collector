9611783714a8b34b6d5a5e9d627178bcc4ccb1e8
jaden4949 <jaden4949@gmail.com>
2024-03-11 13:41:16 -0400

    lab1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Add new view
+def birds_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'birds/index.html', {
+    'birds': birds
+  })
+
+# Add this cats list below the imports
+birds = [
+  {'name': 'Kiwi', 'breed': 'Green cheek conure', 'description': 'cuddly and loving', 'age': 3},
+  {'name': 'Luna', 'breed': 'Cockatiel', 'description': 'intelligent and curious', 'age': 5},
+]
